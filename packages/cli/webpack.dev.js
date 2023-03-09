5c649c1dac218de9da5bca7006cfb931738b2aee
illumination-k <illumination.k.27@gmail.com>
2023-03-10 00:05:23 +0900

    split webpack config

--- packages/cli/webpack.dev.js
+++ packages/cli/webpack.dev.js
@@ -2,7 +2,14 @@ const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-  mode: "production",
+  mode: "development",
+  devtool: "inline-source-map",
+  optimization: {
+    runtimeChunk: true,
+    removeAvailableModules: false,
+    removeEmptyChunks: false,
+    splitChunks: false,
+  },
   module: {
     rules: [
       {
