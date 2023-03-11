c9f1f93bd3bb0c24733f489e9d4c80af28ed4a50
illumination-k <43526984+illumination-k@users.noreply.github.com>
2023-03-12 05:54:43 +0900

    useEffect for ads (#12)
    * fix
    
    * useeffect
    
    * ads not support afeteriteractive?
    
    * ads
    
    * useeffect

--- packages/web/src/components/Adsense.tsx
+++ packages/web/src/components/Adsense.tsx
@@ -1,16 +1,23 @@
 import { apply, tw } from "@twind/core";
+import { useRouter } from "next/router";
 import Script from "next/script";
 import React, { useEffect } from "react";
 
 export default function Adsense({ className }: { className?: string }) {
+  const { asPath } = useRouter();
   const props = process.env.NEXT_PUBLIC_IS_LOCALHOST
     ? {
       "data-adtest": "on",
     }
     : {};
 
+  useEffect(() => {
+    // @ts-ignore
+    (window.adsbygoogle = window.adsbygoogle || []).push({});
+  }, [asPath]);
+
   return (
-    <div className={className}>
+    <div className={className} key={asPath}>
       <ins
         className={tw(apply("adsbygoogle block text-center"))}
         data-ad-client="ca-pub-3483824909024831"
@@ -19,9 +26,6 @@ export default function Adsense({ className }: { className?: string }) {
         data-full-width-responsive="true"
         {...props}
       >
-        <Script id="googleads-script">
-          {`(window.adsbygoogle = window.adsbygoogle || []).push({});`}
-        </Script>
       </ins>
     </div>
   );
