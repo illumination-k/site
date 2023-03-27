748bd6c53dacc3af82c666cf835b719649501231
illumination-k <illumination.k.27@gmail.com>
2023-03-28 01:33:22 +0900

    use swc-loader

--- packages/cli/webpack.config.js
+++ packages/cli/webpack.config.js
@@ -3,14 +3,4 @@ const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
   mode: "production",
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: ["ts-loader"],
-        exclude: /node_modules/,
-      },
-      { test: /\.node$/, use: "node-loader" },
-    ],
-  },
 });
