361b4662871ca3dbd2f90d9d891527188cfae838
illumination-k <skawamura27@preferred.jp>
2024-09-22 16:46:33 +0900

    ssg

--- web/next.config.js
+++ web/next.config.js
@@ -1,23 +1,9 @@
-const path = require("path");
-
-const redirectsData = require("./next.config.redirects");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ["md-plugins"],
-  async redirects() {
-    return redirectsData;
-  },
-  output: "standalone",
+  output: "export",
   experimental: {
-    // @link https://nextjs.org/docs/advanced-features/output-file-tracing#caveats
-    outputFileTracingRoot: path.join(__dirname, "../"),
-
-    // Experimental monorepo support
-    // @link {https://github.com/vercel/next.js/pull/22867|Original PR}
-    // @link {https://github.com/vercel/next.js/discussions/26420|Discussion}
-    externalDir: true,
     typedRoutes: true,
   },
 };
