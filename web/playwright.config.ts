f78d1084d239d77e940032c82c3d8f3177bad280
illumination-k <illumination.k.27@gmail.com>
2026-03-20 19:44:33 +0900

    e2e ci

--- web/playwright.config.ts
+++ web/playwright.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from "@playwright/test";
 
+const isCI = !!process.env.CI;
+
 export default defineConfig({
   testDir: "./e2e",
   use: {
-    baseURL: "http://localhost:3000",
+    baseURL: isCI ? "http://localhost:8788" : "http://localhost:3000",
   },
   webServer: {
-    command: "pnpm dev:next",
-    url: "http://localhost:3000",
+    command: isCI ? "npx serve out -l 8788" : "pnpm dev:next",
+    url: isCI ? "http://localhost:8788" : "http://localhost:3000",
     reuseExistingServer: true,
   },
 });
